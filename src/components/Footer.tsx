6e3226a53be9772c08231a346d98152c2923e036
Muhammad Shafiq <98705902+ShafiqUsman@users.noreply.github.com>
2023-07-24 09:05:52 +0500

    text-xl

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='h-12 md:h-24 p4 lg:p-20 xl:p-40 text-red-500 flex items-center justify-between'>
-      <Link href={'/'} className='font-bold texxl'>MASSIMO</Link>
+      <Link href={'/'} className='font-bold text-xl'>MASSIMO</Link>
       <p >© All RIGHTS RESERVED.</p >
     </div>
   )
