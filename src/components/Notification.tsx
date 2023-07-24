45526c18830a0e0be5630ed7dd6db377e926ae18
Muhammad Shafiq <98705902+ShafiqUsman@users.noreply.github.com>
2023-07-24 09:04:26 +0500

    1st Day Work

---
+++ src/components/Notification.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Notification = () => {
+  return <div className="h-12 bg-red-500 text-white px-4 flex justify-center items-center text-center text-sm md:text-base cursor-pointer">Free delivery for all orders over Rs.500 Order your food now!</div>;
+};
+
+export default Notification;
